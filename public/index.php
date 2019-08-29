3119a5b76d497527d817aac7ce48b4c2d8959cdc
odan <d.opitz@outlook.com>
2019-08-29 22:18:35 +0200

    Remove unused require statement

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 (static function (ContainerInterface $container): void {
     (new SapiEmitter())
         ->emit($container->get(Router::class)
