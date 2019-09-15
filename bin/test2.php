14ebeaf674834537a53d81b65b055e2eca6c6aac
odan <d.opitz@outlook.com>
2019-09-15 22:01:25 +0200

    Test

---
+++ bin/test2.php
@@ -0,0 +1,20 @@
+<?php
+
+echo 'test2.php';
+
+if (isset($_SERVER['REQUEST_METHOD'])) {
+    echo "Only CLI allowed. Script stopped.\n";
+    exit (1);
+}
+
+/** @var \Psr\Container\ContainerInterface $container */
+$container = require __DIR__ . '/../config/bootstrap.php';
+
+$commands = $container->get('settings')['commands'];
+$application = new \Symfony\Component\Console\Application();
+
+foreach ($commands as $class) {
+    $application->add($container->get($class));
+}
+
+echo 'test2.php done';
